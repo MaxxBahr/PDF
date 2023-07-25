4dd67ab3435cabb4769ef5683255dd4a81999661
MaxxBahr <schnee.bahr@googlemail.com>
2023-07-25 11:01:03 +0200

    Datum kann ausgewählt werden wenn der Text angepasst wird.

--- src/main/java/com/example/pdf/Mail.java
+++ src/main/java/com/example/pdf/Mail.java
@@ -4,11 +4,56 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
-
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
+
 public class Mail extends Application {
+
+    @FXML
+    private ChoiceBox<Integer> dayChoiceBox, monthChoiceBox, yearChoiceBox;
+    @FXML
+    private TextArea textarea;
+
+    public void initialize() {
+
+        for (int i = 1; i <= 30; i++) {
+            dayChoiceBox.getItems().add(i);
+        }
+        for (int z = 1; z <= 12; z++) {
+            monthChoiceBox.getItems().add(z);
+        }
+
+        for (int k = 2023; k <= 2030; k++) {
+            yearChoiceBox.getItems().add(k);
+        }
+
+
+    }
+
+    public void selection(){
+        LocalDate currentDate = LocalDate.now();
+        int currentmonth = currentDate.getMonthValue();
+        int currentDay = currentDate.getDayOfMonth();
+        int currentYear = currentDate.getYear();
+
+        if(monthChoiceBox.getItems().get(0) == currentmonth && yearChoiceBox.getItems().get(0) == currentYear) {
+            for(int i = 0; i < currentDay; i++){
+                dayChoiceBox.getItems().remove(i);
+            }
+        }else{
+            initialize();
+        }
+        if(yearChoiceBox.getItems().get(0) == currentYear){
+            for(int k = 0; k < currentmonth; k++){
+                monthChoiceBox.getItems().remove(k);
+            }
+        }else{
+            initialize();
+        }
+    }
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(MainWindowLaunchGUI.class.getResource("mail.fxml"));
@@ -19,10 +64,7 @@ public class Mail extends Application {
     }
 
     @FXML
-    private TextArea textarea;
-
-    @FXML
-    private void speicherung(){
+    private void speicherung() {
         String text = textarea.getText();
         System.out.println(text);
     }
